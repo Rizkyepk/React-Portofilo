aad498fb0e32dcaf5f32c05742c2f37a48fb66c2
Rizkyepk <rizkykusnady11@gmail.com>
2025-03-04 14:28:03 +0700

    Rep Portofilio

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import './App.css'
+import { LoadingScreen } from './components/LoadingScreen'
+import "./index.css"
+import { Navbar } from './components/Navbar';
+
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  return (
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      <div className={`min-h-screen transition-op duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-grey-100`}>
+
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}></Navbar>
+      </div>
+    </>
+  );
+}
+
+export default App

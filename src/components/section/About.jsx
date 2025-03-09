b4c7f94934b4f92f5942c30cd8f1b9d612b76d8b
Rizkyepk <rizkykusnady11@gmail.com>
2025-03-10 00:55:02 +0700

    fix styling

--- src/components/section/About.jsx
+++ src/components/section/About.jsx
@@ -15,7 +15,7 @@ export const About = () => {
     <section id="about" 
     className="min-h-screen flex items-center justify-center py-20"
     >
-        <div className="max-w-3xl mx-auto p-4">
+        <div className="max-w-3xl mx-auto px-4">
             <h1 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text 
             text-transparent text-center"
             >

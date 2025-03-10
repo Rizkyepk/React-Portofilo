1e7d87d4aae7d41ea1f5ff9e42f45e3492daef2b
Rizkyepk <rizkykusnady11@gmail.com>
2025-03-10 14:20:12 +0700

    add revealonscroll effect and udjust to file section

--- src/components/section/Project.jsx
+++ src/components/section/Project.jsx
@@ -1,95 +1,100 @@
+import { RevealOnScroll } from "../RevealOnScroll"
+
 export const Project = () => {
     return (
         <section
             id="projects"
             className="min-h-screen flex items-center justify-center py-20"
         >
-            <div className="max-w-5xl mx-auto px-4">
-                <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 
+            <RevealOnScroll>
+
+                <div className="max-w-5xl mx-auto px-4">
+                    <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 
                 bg-clip-text text-transparent text-center"
-                >
-                    Featured Projects
-                </h2>
+                    >
+                        Featured Projects
+                    </h2>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
 
-                    <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                        <h3 className="text-xl font-bold mb-2"> Flutter App </h3>
-                        <p className="test-grey-400 mb-4">
-                            This is a Flutter app that I built using Dart. It has a simple UI
-                        </p>
-                        <div>
-                            {["Figma", "Flutter", "fastAPI"].map((tech, key) => (
-                                <span
-                                    key={key}
-                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm 
+                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                            <h3 className="text-xl font-bold mb-2"> Flutter App </h3>
+                            <p className="test-grey-400 mb-4">
+                                This is a Flutter app that I built using Dart. It has a simple UI
+                            </p>
+                            <div>
+                                {["Figma", "Flutter", "fastAPI"].map((tech, key) => (
+                                    <span
+                                        key={key}
+                                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm 
                                     hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0,1)] transition-all"
+                                    >
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+                            <div className="flex justify-between items-center">
+                                <a href="https://github.com/Rizkyepk/hmtk_app-master"
+                                    className="text-blue-400 hover:text-blue-300 transition-colors"
                                 >
-                                    {tech}
-                                </span>
-                            ))}
+                                    View Project -
+                                </a>
+                            </div>
                         </div>
-                        <div className="flex justify-between items-center">
-                            <a href="https://github.com/Rizkyepk/hmtk_app-master"
-                                className="text-blue-400 hover:text-blue-300 transition-colors"
-                            >
-                                View Project -
-                            </a>
-                        </div>
-                    </div>
 
-                    <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                        <h3 className="text-xl font-bold mb-2"> Portofolio Web </h3>
-                        <p className="test-grey-400 mb-4">
-                            This is a web app that I built using react.js, It has a simple UI
-                        </p>
-                        <div>
-                            {["React.js", "Tailwindcss"].map((tech, key) => (
-                                <span
-                                    key={key}
-                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm 
+                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                            <h3 className="text-xl font-bold mb-2"> Portofolio Web </h3>
+                            <p className="test-grey-400 mb-4">
+                                This is a web app that I built using react.js, It has a simple UI
+                            </p>
+                            <div>
+                                {["React.js", "Tailwindcss"].map((tech, key) => (
+                                    <span
+                                        key={key}
+                                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm 
                                     hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0,1)] transition-all"
+                                    >
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+                            <div className="flex justify-between items-center">
+                                <a href="#"
+                                    className="text-blue-400 hover:text-blue-300 transition-colors"
                                 >
-                                    {tech}
-                                </span>
-                            ))}
+                                    View Project -
+                                </a>
+                            </div>
                         </div>
-                        <div className="flex justify-between items-center">
-                            <a href="#"
-                                className="text-blue-400 hover:text-blue-300 transition-colors"
-                            >
-                                View Project -
-                            </a>
-                        </div>
-                    </div>
 
-                    <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
-                        <h3 className="text-xl font-bold mb-2"> UI Web Page </h3>
-                        <p className="test-grey-400 mb-4">
-                            This is a web app that I built using Figma, It has a simple UI
-                        </p>
-                        <div>
-                            {["Figma"].map((tech, key) => (
-                                <span
-                                    key={key}
-                                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm 
+                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                            <h3 className="text-xl font-bold mb-2"> UI Web Page </h3>
+                            <p className="test-grey-400 mb-4">
+                                This is a web app that I built using Figma, It has a simple UI
+                            </p>
+                            <div>
+                                {["Figma"].map((tech, key) => (
+                                    <span
+                                        key={key}
+                                        className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm 
                                     hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0,1)] transition-all"
+                                    >
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+                            <div className="flex justify-between items-center">
+                                <a href="#"
+                                    className="text-blue-400 hover:text-blue-300 transition-colors"
                                 >
-                                    {tech}
-                                </span>
-                            ))}
+                                    View Project -
+                                </a>
+                            </div>
                         </div>
-                        <div className="flex justify-between items-center">
-                            <a href="#"
-                                className="text-blue-400 hover:text-blue-300 transition-colors"
-                            >
-                                View Project -
-                            </a>
-                        </div>
-                    </div>
 
+                    </div>
                 </div>
-            </div>
+            </RevealOnScroll>
         </section>
     )
 }
